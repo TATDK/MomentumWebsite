04dea331691d202624c638b816b378752d011d46
Thomas Andresen <thomas.andresen@earthgame.dk>
2013-02-02 11:11:18 +0100

    push to site

--- _copy to repo.bat
+++ _copy to repo.bat
@@ -1,4 +1,11 @@
 @echo off
 echo ============ JEKYLL ============
-jekyll ../MomentumWebsite --no-server --no-auto
+jekyll
+echo ============ PAGES  ============
+checkout gh-pages
+cp -r _site/* . && rm -rf _site/ && touch .nojekyll
+git add *
+git commit -m "Updated website"
+git push -all origin
+git checkout source
 pause
