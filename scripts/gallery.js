89eec6325e12b9f54b3bfe5fb1e97a214fa0f58b
Thomas Andresen <thomas.andresen@earthgame.dk>
2013-02-22 13:31:03 +0100

    Gallery opacity

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -5,36 +5,62 @@ var gallery = {
         c: [20,330,640],
         d: 0
     },
+    h: [],
     load: function(a) {
-        var c = $("#gallery"),d = a.pop(),e = new Image(),g = this;
-        e.onload = function() {
+        if (a) this.h = this.h.concat(a);
+        if (this.h.length == 0) return;
+        var b = this.h.splice(0,1)[0];
+        if (b.type === 'image')
+            this.loadImage(b.src,b.filetype);
+        else if (b.type === 'audio')
+            this.loadAudio(b.src,b.filetype);
+        else if (b.type === 'video')
+            this.loadVideo(b.src,b.filetype);
+        else console.error('Unknown type',b.type,b);
+    },
+    loadImage: function(a,b) {
+        var c = $('#gallery'),d = new Image(),e = this;
+        d.onload = function() {
             var f = $("<a></a>")
-                    .attr('href','/images/gallery/' + d + '.jpg')
+                    .attr('href','/images/gallery/' + a + '.' + b)
                     .attr('rel','lightbox[gallery]')
-                    .css('top',g.b.a[g.b.b]+'px')
-                    .css('left',g.b.c[g.b.b]+'px')
-                    .append(e);
-            g.b.a[g.b.b] += this.height+10;
-            if (g.b.d < g.b.a[g.b.b]) {
-                g.b.d = g.b.a[g.b.b]+10
-                c.css('height',g.b.d+'px');
+                    .css('top',e.b.a[e.b.b]+'px')
+                    .css('left',e.b.c[e.b.b]+'px')
+                    .append(d);
+            e.b.a[e.b.b] += this.height+10;
+            if (e.b.d < e.b.a[e.b.b]) {
+                e.b.d = e.b.a[e.b.b]+10
+                c.css('height',e.b.d+'px');
             }
-            g.b.b++;
-            if (g.b.b == 3) g.b.b = 0;
+            e.b.b++;
+            if (e.b.b == 3) e.b.b = 0;
             c.append(f);
-            if (a.length > 0) g.load(a);
+            e.load();
         };
-        e.src = "/images/gallery/" + d + "_thumb.jpg";
+        d.src = '/images/gallery/' + a + '_thumb.' + b;
+    },
+    loadAudio: function(a,b) {
+        var c = $('#gallery'),d = $('<audio></audio>').attr('src','/audio/' + a + '.' + b).append($('<p></p>').text('Your browser does not support the audio element'));
+        c.append(d);
+        this.load();
+    },
+    loadVideo: function(a,b) {
+        var c = $('#gallery'),d = $('<video></video>');
+        for (var i in b)
+            d.append($('<source>').attr('src','/video/' + a + '.' + b[i]).attr('type','video/' + b[i]));
+        d.append($('<p></p>').text('Your browser does not support the video element'));
+        c.append(d);
+        this.load();
     }
 };
 $(document).ready(function() {
     gallery.load([
-        'smallRobot',
-        'robot',
-        'conceptCharacter3',
-        'conceptCharacter2',
-        'conceptCharacter1',
-        'charactersDevice',
-        'characterDevices'
+        { src: 'characterDevices',  filetype: 'jpg', type: 'image' },
+        { src: 'charactersDevice',  filetype: 'jpg', type: 'image' },
+        { src: 'conceptCharacter1', filetype: 'jpg', type: 'image' },
+        { src: 'conceptCharacter2', filetype: 'jpg', type: 'image' },
+        { src: 'conceptCharacter3', filetype: 'jpg', type: 'image' },
+        { src: 'robot',             filetype: 'jpg', type: 'image' },
+        { src: 'smallRobot',        filetype: 'jpg', type: 'image' }
     ]);
 });
